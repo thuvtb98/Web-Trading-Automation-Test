d075513a6060b06495dcc377ef3b6b82fb02d98b
Admin <thubichvi98@gmail.com>
2023-06-18 21:34:40 +0700

    thu commit at home

--- src/main/java/Tests/login.java
+++ src/main/java/Tests/login.java
@@ -7,6 +7,7 @@ import Ultilities.URL;
 import java.io.File;
 import model.pages.HomePage;
 import model.pages.LoginPage;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -14,50 +15,43 @@ import org.openqa.selenium.WebDriver;
 import org.testng.asserts.SoftAssert;
 
 public class login extends driverBase {
-//  @Test(dataProvider = "PassLoginData", dataProviderClass = DataProviderFactory.class)
-//  public void LoginWithCorrectCredential(String username, String password, String afacctno){
-//    WebDriver driver = getChromeDriverInstance();
-//    String TestUrl = URL.currentTestUrl("LOGIN_PAGE");
-//    driver.get(TestUrl);
-//    SoftAssert softAssert = new SoftAssert();
-//
-//    LoginPage loginPage = new LoginPage(driver);
-//    loginPage
-//        .clearUsername()
-//        .inputUsername(username)
-//        .inputPassword(password)
-//        .clickLoginBtn();
-//
-//    HomePage homePage = new HomePage(driver);
-//    softAssert.assertTrue(homePage.getLogoWebElem().isDisplayed(), "The company's logo is not displayed!");
-//    Assert.assertEquals(homePage.selectAf().getFirstSelectedOption().getText(), afacctno);
-//}
+  //  @Test(dataProvider = "PassLoginData", dataProviderClass = DataProviderFactory.class)
+  //  public void LoginWithCorrectCredential(String username, String password, String afacctno){
+  //    WebDriver driver = getChromeDriverInstance();
+  //    String TestUrl = URL.currentTestUrl("LOGIN_PAGE");
+  //    driver.get(TestUrl);
+  //    SoftAssert softAssert = new SoftAssert();
+  //
+  //    LoginPage loginPage = new LoginPage(driver);
+  //    loginPage
+  //        .clearUsername()
+  //        .inputUsername(username)
+  //        .inputPassword(password)
+  //        .clickLoginBtn();
+  //
+  //    HomePage homePage = new HomePage(driver);
+  //    softAssert.assertTrue(homePage.getLogoWebElem().isDisplayed(), "The company's logo is not
+  // displayed!");
+  //    Assert.assertEquals(homePage.selectAf().getFirstSelectedOption().getText(), afacctno);
+  // }
 
   @Test(dataProvider = "FailLoginData", dataProviderClass = DataProviderFactory.class)
-  public void LoginWithInCorrectCredential(String username, String password, String alert){
+  public void LoginWithInCorrectCredential(String username, String password, String alert)
+      throws InterruptedException {
     WebDriver driver = getChromeDriverInstance();
     String TestUrl = URL.currentTestUrl("LOGIN_PAGE");
     driver.get(TestUrl);
     SoftAssert softAssert = new SoftAssert();
     LoginPage loginPage = new LoginPage(driver);
 
-    loginPage
-        .clearUsername()
-        .inputUsername(username)
-        .inputPassword(password)
-        .clickLoginBtn();
+    loginPage.clearUsername().inputUsername(username).inputPassword(password).clickLoginBtn();
 
-    int lenght = loginPage.getValidationErrors().size();
-    System.out.println(lenght);
-//    switch (lenght){
-//      case 0:
-//        Assert.assertEquals(loginPage.getValidationErrors().get(0).getText(), alert);
-//        break;
-//      case 1:
-//        Assert.assertEquals(loginPage.getValidationErrors().get(0).getText().concat(loginPage.getValidationErrors().get(1).getText()), alert);
-//        break;
-//    }
-//    lenght = 0;
-  }
+    Thread.sleep(2000);
 
+    String liValue = "";
+    for (WebElement li : loginPage.getValidationErrors()) {
+      liValue = liValue.concat(li.getText());
+    }
+    Assert.assertEquals(liValue, alert);
+    }
 }
